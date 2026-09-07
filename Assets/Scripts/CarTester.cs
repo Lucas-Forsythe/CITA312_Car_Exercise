14836a74d7c57233324fd0b6227429871f6246ba
Lucas Forsythe <lucasforsythe2004@gmail.com>
2026-09-07 18:21:11 -0400

    Exercise 4 - Rigidbody and collision
    Completed Exercise 4 by following lab and adding my own hit count.

--- Assets/Scripts/CarTester.cs
+++ Assets/Scripts/CarTester.cs
@@ -2,9 +2,29 @@ using UnityEngine;
 
 public class CarTester : MonoBehaviour
 {
+    private int hitcount = 0;
+    //A rigidbody is a class component that allows a GameObject to be affected by physics.
+    // Three property names are mass, use gravity, and linear damping.
+    private Rigidbody rb;
     public float moveSpeed = 1000f;
 
     public float turnSpeed = 100f;
+    
+    void Start()
+    {
+        // rb is a varaible that references the specific rigidbody component attached to whatever has the CarTester script attached to it.
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.mass = 2f;
+        //rb.AddForce(0,0,10000f);
+        /*This sent the car flying off the screen.
+        rb.AddForce(1000f, 0, 1000f, ForceMode.Impulse);
+        */
+        /*This sent the car moving but did not ahve the same strenght.
+        rb.AddForce(1000f, 0, 1000f, ForceMode.Acceleration);
+        Mass can affect forces but only force and impulse.
+        */
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,7 +34,7 @@ public class CarTester : MonoBehaviour
 
         if (Input.GetAxis("Vertical") != 0)
         {
-            Debug.Log("Car is moving");
+            //Debug.Log("Car is moving");
         }
     }
 
@@ -39,7 +59,20 @@ public class CarTester : MonoBehaviour
         {
             transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
         }
+    }
 
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        hitcount++;
+        //Void means return nothing
+        //OnCollisionEnter is a built in unity function that is called when two colliders touch
+        //Collision is a class that has information such as point of collision and what hit what
+        //collision is the variable that is passed into our function
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Obstacle Detected");
+        }
+        Debug.Log("Car has collided with " + collision.gameObject.name);
+        Debug.Log("Car has collided " + hitcount + " times");
     }
 }
